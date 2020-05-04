5966a90a4cf908b8a7aeb943d09aad94952d9aed
Luke Bennetts <luke.bennetts@adelaide.edu.au>
2020-05-04 12:02:58 +0930

    Update plotting in thinVsFull.m

--- examples/thinVsFull.m
+++ examples/thinVsFull.m
@@ -5,6 +5,14 @@ clear
 clc
 global ff
 
+my_path=which('thinVsFull.m');
+my_path=my_path(1:end-13);
+
+if ~strcmp(pwd,my_path)
+ hld_path=pwd;
+ cd(my_path)
+end
+
 %% Get the Parameters of the ice.
 [~,~,~,~,E,nu,rhow,rhoi,g,~] = getProperties();
 H = 800;
@@ -92,10 +100,12 @@ xlim([0,LL]);
 xticks([0,LL/4,LL/2,3*LL/4,LL]);
 xticklabels([0,L/4000,L/2000,3*L/4000,L/1000]);
 axis equal
-ylabel('$\mathbf{w}(x,z)$');
-xlabel('$x$ (in km)');
+ylabel('$\mathbf{w}(x,z)$ [m]');
+xlabel('$x$ [km]');
 hold on
 grid on
+
+legend(sp1,'Full elasticity (modal method)','Thin plate');
 
 % DirectFEM
 sp2=subplot(2,1,2);
@@ -115,6 +125,10 @@ xticklabels([0,L/4000,L/2000,3*L/4000,L/1000]);
 axis equal
 ylabel('$\mathbf{w}(x,z)$');
 xlabel('$x$ (in km)');
+
+legend(sp2,'Full elasticity (direct FEM)','Thin plate');
+
+set(figure(1),'Name','Vertical deflection of shelf-cavity interface')
 
 %% Compute the velocity potentials
 [pts3,seg3,tri3] = importfilemesh('1_Forced/2_Deformation/cavityMesh.msh');
@@ -139,9 +153,11 @@ xticklabels([0,L/4000,L/2000,3*L/4000,L/1000]);
 ylim([-HH,-dd]);
 yticks([-HH,-(HH+dd)/2,-dd]);
 yticklabels([-H,-(H+d)/2,-d]);
-xlabel('$x$ (in km)');
-ylabel('$z$');
+xlabel('$x$ [km]');
+ylabel('$z$ [m]');
 grid on
+
+title(sp3,'Velocity potential Re($\phi(x,z)$) (modal method)')
 
 sp4=subplot(2,1,2);
 pdeplot(pts3,seg3,tri3,'XYData',real(PHIEB)','colormap','jet');
@@ -155,6 +171,10 @@ yticklabels([-H,-(H+d)/2,-d]);
 ylabel('$z$');
 xlabel('$x$ (in km)');
 grid on
+
+title(sp4,'Velocity potential Re($\phi(x,z)$) (thin plate)')
+
+set(figure(2),'Name','Velocity potential in sub-shelf water cavity')
 
 %% Save figure file (Uncomment to save)
 % saveas(fig,['Compare1T',num2str(T),'.fig'],'fig');
@@ -202,7 +222,7 @@ hold on
 pp2=pdeplot(ptsC,segC,triC);
 set(pp2,'Color','b');
 axis equal
-xlabel('$x$ (in km)');
+xlabel('$x$ [km]');
 ylabel('$z/L_c$');
 xlim([0-LL/10,0+LL/5]);
 xticks([0-LL/10,0+LL/5]);
@@ -219,13 +239,23 @@ hold on
 pp2=pdeplot(ptsC,segC,triC);
 set(pp2,'Color','b');
 axis equal
-xlabel('$x$ (in km)');
+xlabel('$x$ [km]');
 ylabel('$z/L_c$');
 xlim([LL-LL/5,LL]);
 xticks([LL-LL/5,LL]);
 xticklabels(1/1000*[L-L/5,L]);
 grid on
 hold on
+
+set(fig10,'Name','Meshes')
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if exist('hld_path','var')
+ cd(hld_path)
+ clear hld_path
+end
 
 % Uncomment to write to a file.
 %export_fig('meshesUni.pdf','-pdf','-transparent',fig10);
