abe1951b6e49ac6644e308693f242bcaa7d655c8
Balaje <balaje6@gmail.com>
2020-07-15 08:00:21 +1000

    Modify the scripts

--- genJobScript.sh
+++ genJobScript.sh
@@ -9,15 +9,20 @@ echo "module load freefem" >> job.sh
 echo "cd \$PBS_O_WORKDIR" >> job.sh
 echo "export FF_INCLUDEPATH=\"\$PBS_O_WORKDIR/include\"" >> job.sh
 var=$(echo "$1*$2" | bc -l)
-if [[ "$3"=="--default" ]]; then
-    echo "mpirun -np $var FreeFem++-mpi -nw -v 0 simple5.edp -L 20000 -H 800 -h 200 -Tr 200 -Ti 0 -iter 0" >> job.sh
+
+if [ "$3" == "-default" ]; then    
+    echo "mpirun -np $var FreeFem++-mpi -nw -v 0 simple5.edp -iter 0" >> job.sh
+elif [ "$3" == "solveBEDMAP2.edp" ]; then
+    echo "mpirun -np $var FreeFem++-mpi -nw -v 0 $3 -isMesh $4 -nborders $5 -Tr 6 -Ti $7 -iter $8" >> job.sh    
+elif [ "$3" == "-splitDefault" ]; then
+    echo "mpirun -np $var FreeFem++-mpi -nw -v 0 splitMesh.edp -hsize 0.02" >> job.sh    
+elif [ "$3" == "splitMesh.edp" ]; then
+    echo "mpirun -np $var FreeFem++-mpi -nw -v 0 $3 -hsize $10 -isBEDMAP2 $11 -L $4 -H $5 -h $6 -Tr $7 -Ti $8 -iter $9" >> job.sh  
 else
-    if [[ "$3"=="solveBEDMAP2.edp" ]]; then
-        echo "mpirun -np $var FreeFem++-mpi -nw -v 0 $3 -isMesh $4 -nborders $5 -Tr 6 -Ti $7 -iter $8" >> job.sh
-    else
-        echo "mpirun -np $var FreeFem++-mpi -nw -v 0 $3 -L $4 -H $5 -h $6 -Tr $7 -Ti $8 -iter $9" >> job.sh
-    fi        
-fi   
+    echo "mpirun -np $var FreeFem++-mpi -nw -v 0 $3 -L $4 -H $5 -h $6 -Tr $7 -Ti $8 -iter $9" >> job.sh
+fi
+
+
 echo "exit 0" >> job.sh
 
 cat job.sh
