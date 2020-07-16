e9b5ff2e383adf6f908ce2bc32658a51d587a205
Balaje K [HPC] <balaje6@gmail.com>
2020-07-16 10:58:46 +1000

    Commit changes

--- genJobScript.sh
+++ genJobScript.sh
@@ -15,9 +15,11 @@ if [ "$3" == "-default" ]; then
 elif [ "$3" == "solveBEDMAP2.edp" ]; then
     echo "mpirun -np $var FreeFem++-mpi -nw -v 0 $3 -isMesh $4 -nborders $5 -Tr 6 -Ti $7 -iter $8" >> job.sh    
 elif [ "$3" == "-splitDefault" ]; then
-    echo "mpirun -np $var FreeFem++-mpi -nw -v 0 splitMesh.edp -hsize 0.02" >> job.sh    
+    rm -vf ./MESHES_SPLIT/*.dat ./MESHES_SPLIT/*.msh
+    echo "mpirun -np $var FreeFem++-mpi -nw -v 0 splitMesh.edp -hsize $4" >> job.sh    
 elif [ "$3" == "splitMesh.edp" ]; then
-    echo "mpirun -np $var FreeFem++-mpi -nw -v 0 $3 -hsize $10 -isBEDMAP2 $11 -L $4 -H $5 -h $6 -Tr $7 -Ti $8 -iter $9" >> job.sh  
+    rm -vf ./MESHES_SPLIT/*.dat ./MESHES_SPLIT/*.msh
+    echo "mpirun -np $var FreeFem++-mpi -nw -v 0 $3 -hsize ${10} -isBEDMAP2 ${11} -L $4 -H $5 -h $6 -Tr $7 -Ti $8 -iter $9" >> job.sh  
 else
     echo "mpirun -np $var FreeFem++-mpi -nw -v 0 $3 -L $4 -H $5 -h $6 -Tr $7 -Ti $8 -iter $9" >> job.sh
 fi
