cda6e1fb2f00b52b8a31ffd80c0b17440779b214
Balaje K [HPC] <balaje6@gmail.com>
2020-07-16 12:03:23 +1000

    Commit file jobscript

--- genJobScript.sh
+++ genJobScript.sh
@@ -13,13 +13,17 @@ var=$(echo "$1*$2" | bc -l)
 if [ "$3" == "-default" ]; then    
     echo "mpirun -np $var FreeFem++-mpi -nw -v 0 simple5.edp -iter 0" >> job.sh
 elif [ "$3" == "solveBEDMAP2.edp" ]; then
-    echo "mpirun -np $var FreeFem++-mpi -nw -v 0 $3 -isMesh $4 -nborders $5 -Tr 6 -Ti $7 -iter $8" >> job.sh    
+    echo "mpirun -np $var FreeFem++-mpi -nw -v 0 $3 -isMesh $4 -nborders $5 -Tr $6 -Ti $7 -iter $8" >> job.sh    
 elif [ "$3" == "-splitDefault" ]; then
     rm -vf ./MESHES_SPLIT/*.dat ./MESHES_SPLIT/*.msh
     echo "mpirun -np $var FreeFem++-mpi -nw -v 0 splitMesh.edp -hsize $4" >> job.sh    
 elif [ "$3" == "splitMesh.edp" ]; then
     rm -vf ./MESHES_SPLIT/*.dat ./MESHES_SPLIT/*.msh
-    echo "mpirun -np $var FreeFem++-mpi -nw -v 0 $3 -hsize ${10} -isBEDMAP2 ${11} -L $4 -H $5 -h $6 -Tr $7 -Ti $8 -iter $9" >> job.sh  
+    if [ "${11}" == "1" ]; then
+	echo "mpirun -np $var FreeFem++-mpi -nw -v 0 $3 -hsize ${10} -isBEDMAP2 ${11} -iter $9" >> job.sh
+    else
+	echo "mpirun -np $var FreeFem++-mpi -nw -v 0 $3 -hsize ${10} -isBEDMAP2 ${11} -L $4 -H $5 -h $6 -Tr $7 -Ti $8 -iter $9" >> job.sh
+    fi    
 else
     echo "mpirun -np $var FreeFem++-mpi -nw -v 0 $3 -L $4 -H $5 -h $6 -Tr $7 -Ti $8 -iter $9" >> job.sh
 fi
