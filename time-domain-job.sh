67d652310535b6f996a5a9054a3d913144373bc3
Balaje K [HPC] <balaje6@gmail.com>
2020-08-10 21:35:39 +1000

    Changes

--- time-domain-job.sh
+++ time-domain-job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PBS -l select=1:ncpus=10:mpiprocs=10:mem=32GB
+#PBS -l select=1:ncpus=20:mpiprocs=20:mem=32GB
 #PBS -l walltime=10:00:00
 #PBS -k oe
 
@@ -8,5 +8,5 @@ source /etc/profile.d/modules.sh
 module load freefem
 cd $PBS_O_WORKDIR
 export FF_INCLUDEPATH="$PBS_O_WORKDIR/include"
-mpirun -np 10 FreeFem++-mpi -nw -v 0 timeDomain.edp
+mpirun -np 20 FreeFem++-mpi -nw -v 0 timeDomain.edp
 exit 0
