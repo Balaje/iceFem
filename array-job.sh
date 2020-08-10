d9eff34eb484cc72083f6927abefc56760d10b67
Balaje <balaje6@gmail.com>
2020-08-10 14:13:28 +1000

    Modify array-job

--- array-job.sh
+++ array-job.sh
@@ -3,18 +3,19 @@
 #PBS -l select=1:ncpus=8:mpiprocs=8:mem=16GB
 #PBS -l walltime=100:00:00
 #PBS -k oe
-#PBS -J 0-50
+#PBS -J 0-49
+#PBS -q testq
 
 JOBS=50
 TMIN=$(echo 1/0.0001 | bc -l)
 TMAX=$(echo 1/0.01 | bc -l)
 
-STEP=$(echo \($TMAX-$TMIN\)/$JOBS | bc -l)
+STEP=$(echo \($TMAX-$TMIN\)/\($JOBS-1\) | bc -l)
 TVAL=$(echo $TMIN+$PBS_ARRAY_INDEX*$STEP | bc -l)
 
 source /etc/profile.d/modules.sh
 module load freefem
 cd $PBS_O_WORKDIR
 export FF_INCLUDEPATH="$PBS_O_WORKDIR/include"
-mpirun -np 8 FreeFem++-mpi -nw -v 0 solveBEDMAP2.edp -isMesh 0 -nborders 6 -Tr $TVAL -Ti 0 -iter $PBS_ARRAY_INDEX
+echo mpirun -np 8 FreeFem++-mpi -nw -v 0 solveBEDMAP2.edp -isMesh 0 -nborders 6 -Tr $TVAL -Ti 0 -iter $(( $PBS_ARRAY_INDEX+1 ))
 exit 0
