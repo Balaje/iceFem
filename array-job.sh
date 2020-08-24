2fe839b3be2c0f0f9b7ad6491bf58cb1dea0c1fc
Balaje K [HPC] <balaje6@gmail.com>
2020-08-24 11:28:28 +1000

    Chage ff++ version

--- array-job.sh
+++ array-job.sh
@@ -16,7 +16,7 @@ OMEGAVAL=$(echo $OMEGAMIN+$PBS_ARRAY_INDEX*$STEP | bc -l)
 TVAL=$(echo 2*$PI/$OMEGAVAL | bc -l)
 
 source /etc/profile.d/modules.sh
-module load freefem
+module load freefem/4.4.2
 cd $PBS_O_WORKDIR
 export FF_INCLUDEPATH="$PBS_O_WORKDIR/include"
 mpirun -np 8 FreeFem++-mpi -nw -v 0 solveBEDMAP2.edp -isMesh 0 -nborders 6 -Tr $TVAL -Ti 0 -iter $(( $PBS_ARRAY_INDEX+1 ))
