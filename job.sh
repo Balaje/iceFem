2d0aac1c27a4b7e0935a616265d7da37bb777cb3
Balaje K [HPC] <balaje6@gmail.com>
2020-09-29 12:38:35 +1000

    Modify job scripts

--- job.sh
+++ job.sh
@@ -8,6 +8,6 @@ source /etc/profile.d/modules.sh
 module load freefem
 cd $PBS_O_WORKDIR
 export FF_INCLUDEPATH="$PBS_O_WORKDIR/include"
-#mpirun -np 4 FreeFem++-mpi -nw -v 0 solveBEDMAP2.edp -isMesh 1 -hsize 0.02 -nborders 6 -Tr 10000 -Ti 0 -iter 0 -notchWidth 0.002 -notchHeight 0.0
-mpirun -np 8 FreeFem++-mpi -nw -v 0 simple5.edp -Tr 50 -Ti 0 -iter 0 -hsize 0.01 -N 10 -isSplit 1 -notchWidth 0.005 -notchHeight 0.0
+mpirun -np 8 FreeFem++-mpi -nw -v 0 solveBEDMAP2.edp -isMesh 1 -hsize 0.02 -nborders 6 -Tr 10000 -Ti 0 -iter 0 -notchWidth 0.005 -notchHeight 0.8
+#mpirun -np 8 FreeFem++-mpi -nw -v 0 simple5.edp -Tr 50 -Ti 0 -iter 0 -hsize 0.01 -N 10 -isSplit 1 -notchWidth 0.005 -notchHeight 0.0
 exit 0
--- time-domain-job.sh
+++ time-domain-job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PBS -l select=1:ncpus=8:mpiprocs=8:mem=32GB
+#PBS -l select=1:ncpus=8:mpiprocs=8:mem=8GB
 #PBS -l walltime=10:00:00
 #PBS -k oe
 
