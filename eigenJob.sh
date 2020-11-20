a3adf50aa1d14466b0053068e553af6ce4f20a9f
Balaje <balaje6@gmail.com>
2020-11-20 12:37:42 +1100

    Add new files for solving eigenvalue problems

---
+++ eigenJob.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#PBS -l select=1:ncpus=16:mpiprocs=16:mem=16GB
+#PBS -l walltime=10:00:00
+#PBS -k oe
+
+source /etc/profile.d/modules.sh
+module load freefem
+cd $PBS_O_WORKDIR
+export FF_INCLUDEPATH="$PBS_O_WORKDIR/include"
+mpirun -np 2 FreeFem++-mpi -v 0 eigenSolve.edp -hsize 0.01 -N 12 -nev 64
+#mpirun -np 2 FreeFem++-mpi -v 0 eigenSolve.edp -hsize 0.01 -N 12 -nev 64 -isBEDMAP 1 -isMesh 1
+
