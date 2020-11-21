2ed8e801cbe0bfc753e27ef4b4e8fc1553659a30
Balaje <balaje6@gmail.com>
2020-11-21 14:23:22 +1100

    Changes

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PBS -l select=1:ncpus=16:mpiprocs=16:mem=16GB
+#PBS -l select=2:ncpus=8:mpiprocs=8:mem=16GB
 #PBS -l walltime=10:00:00
 #PBS -k oe
 
@@ -8,6 +8,6 @@ source /etc/profile.d/modules.sh
 module load freefem
 cd $PBS_O_WORKDIR
 export FF_INCLUDEPATH="$PBS_O_WORKDIR/include"
-mpirun -np 16 FreeFem++-mpi -v 0 simple5.edp -isSplit 1 -iter 0 -isUniRef 1 -hsize 0.01 -nev 64 -N 12
+mpirun -np 16 –f $PBS_NODEFILE FreeFem++-mpi -v 0 simple5.edp -isSplit 1 -iter 0 -isUniRef 1 -hsize 0.01 -nev 64 -N 12
 #mpirun -np 16 FreeFem++-mpi -v 0 solveBEDMAP2.edp -nborders 6 -isMesh 1 -isSplit 1 -isUniRef 1 -iter 0 -hsize 0.05 -nev 64
 exit 0
