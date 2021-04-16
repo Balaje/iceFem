d9d96f848ea2bdcb07db13e2707af8f5bd868321
Balaje K <balaje6@gmail.com>
2021-04-16 12:01:04 +1000

    New version working till splitting

--- genDir.sh
+++ genDir.sh
@@ -2,11 +2,12 @@
 
 dirName=$1
 
-mkdir -v $dirName
+
+mkdir -p $dirName
 cd $dirName
-mkdir -v 2_Deformation 2_Modes 2_ModesMatrix 2_Potential 2_RefCoeff 2_Stresses
+mkdir -p 2_Deformation 2_Modes 2_ModesMatrix 2_Potential 2_RefCoeff 2_Stresses
 cd 2_ModesMatrix
-mkdir -v Interpolated_F Interpolated_H Interpolated_L
+mkdir -p Interpolated_F Interpolated_H Interpolated_L
 cd ..
 cd 2_RefCoeff
-mkdir -v Interpolated_R RefCoeff_Dif RefCoeff_Rad
+mkdir -p Interpolated_R RefCoeff_Dif RefCoeff_Rad
